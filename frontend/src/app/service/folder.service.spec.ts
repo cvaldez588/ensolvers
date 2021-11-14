b615adfb7c2b5e951f68e1682b437512db4aad6d
Cecilia Valdez <cecilia.l.valdez@accenture.com>
2021-11-14 11:33:03 -0300

    Adding unit test

--- frontend/src/app/service/folder.service.spec.ts
+++ frontend/src/app/service/folder.service.spec.ts
@@ -1,16 +1,67 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
 
 import { FolderService } from './folder.service';
 
+class MockHttpClient {
+  get(url: string, options?: any): Observable<any> {
+    return of('response');
+  };
+  post(url: string, body: any, options?: any): Observable<any> {
+    return of('response');
+  }
+  delete(url: string): Observable<any> {
+    return of('response');
+  } 
+}
 describe('FolderserviceService', () => {
   let service: FolderService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useClass: MockHttpClient },
+      ]
+    });
     service = TestBed.inject(FolderService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getAll method should call get Http method', (done: DoneFn) => {
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'get').and.callThrough();
+
+    service.getAll().subscribe(r => {
+      expect(httpClient.get).toHaveBeenCalled();
+      done();
+    })
+  });
+  it('save method should call post Http method', (done: DoneFn) => {
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'post').and.callThrough();
+
+    const folder = {
+      folder_id: 1,
+      description: 'description',
+      last_update: Date
+    }
+    service.save(folder).subscribe(r => {
+      expect(httpClient.post).toHaveBeenCalled();
+      done();
+    })
+  });
+  it('delete method should call delete Http method', (done: DoneFn) => {
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'delete').and.callThrough();
+
+    service.delete(1).subscribe(r => {
+      expect(httpClient.delete).toHaveBeenCalled();
+      done();
+    })
   });
 });
--- frontend/src/app/service/work.service.spec.ts
+++ frontend/src/app/service/work.service.spec.ts
@@ -1,16 +1,65 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs/internal/observable/of';
 import { WorkService } from './work.service';
+import { Observable } from 'rxjs';
+import { IWork } from '../components/work/model/work';
 
+class MockHttpClient {
+  get(url: string, options?: any): Observable<any> {
+      return of('response');
+  };
+  post(url: string, body: any, options?: any): Observable<any> {
+      return of('response');
+  }
+}
 describe('WorkService', () => {
   let service: WorkService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useClass: MockHttpClient },
+      ]
+    });
     service = TestBed.inject(WorkService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('findById method should call get Http method', (done: DoneFn) => {
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'get').and.callThrough();
+
+    service.find(1).subscribe(r => {
+      expect(httpClient.get).toHaveBeenCalled();
+      done();
+    })
+  });
+  it('findbyfolder method should call get Http method', (done: DoneFn) => {
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'get').and.callThrough();
+
+    service.findbyfolder(1).subscribe(r => {
+      expect(httpClient.get).toHaveBeenCalled();
+      done();
+    })
+  });
+  it('save method should call get Http method', (done: DoneFn) => {
+    const httpClient = TestBed.inject(HttpClient);
+    spyOn(httpClient, 'post').and.callThrough();
+
+    const work = {
+      work_id: 1,
+      description: 'description',
+      mark: true,
+      folder_id: 1,
+      last_update: new Date()
+    }
+    service.save(work).subscribe(r => {
+      expect(httpClient.post).toHaveBeenCalled();
+      done();
+    })
   });
 });
